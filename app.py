f2ee7f869721e1fb8fa5a598c196379f96609294
Jathavaan Shankarr <jathavaan12@gmail.com>
2023-04-01 22:23:52 +0200

    refactor: Minor changes

--- app.py
+++ app.py
@@ -2,3 +2,4 @@ from src.script import main
 
 if __name__ == "__main__":
     main()
+
--- src/script.py
+++ src/script.py
@@ -6,6 +6,7 @@ def main() -> None:
     las = LAS(file_path=Config.SPEEDBUMP_DATA_PATH.value)
     las.flag_LAS()
     flagged_LAS = [l for l in las.segmented_LAS if l.flagged]
+    [print(l.SD(l.point_cloud)) for l in flagged_LAS]
     merged_pc = las.merge_segmented_pc(*flagged_LAS)
     las.display(merged_pc)
 
